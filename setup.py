04a15f41098ec643e2982b15e71c36dd64465c53
tombuente <100965650+tombuente@users.noreply.github.com>
2022-03-08 20:46:03 +0100

    add author and url

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="wifi-qr",
     version="0.1.0",
     packages=find_packages(),
+    author="Tom Buente",
+    url="https://github.com/tombuente/wifi-qr",
     include_package_data=True,
     install_requires=[
         "Click",
